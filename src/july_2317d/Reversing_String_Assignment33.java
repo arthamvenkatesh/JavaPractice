bfd45fd2b4bd51d112660b2e1c1892dbc607bd1e
venkat <abc@vg.com>
2024-07-23 16:23:39 +0530

    updated 23rd for Assignment 33

---
+++ src/july_2317d/Reversing_String_Assignment33.java
@@ -0,0 +1,12 @@
+package july_2317d;
+
+public class Reversing_String_Assignment33 {
+
+	public static void main(String[] args) {
+		String s = "Venkatesh";
+		for (int i = 8; i >= 0; i--) {
+			char c = s.charAt(i);
+			System.out.print(c);
+		}
+	}
+}
